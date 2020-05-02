b9be679eb43aa7732bf7b452f66bc8a6c8fa77e3
hiroyannnn <hiroyannnn@gmail.com>
2020-05-02 17:13:53 +0900

    next-redux

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Counter from '../components/Counter'
 
 export default () => (
   <Layout header="Next" title="Top page.">
-    <p>Welcome to next.js!</p>
+    <Counter />
     <hr />
+    <div>
       <Link href="/other">
         <a>Go to Other page &gt;&gt;</a>
       </Link>
+    </div>
   </Layout>
 )
